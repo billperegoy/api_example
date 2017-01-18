880113b784ac07fb92473fdfb48f887f423f13a2
Bill Peregoy <bill@peregoy.org>
2017-01-18 18:43:51 -0500

    Added proper status return to user controller.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -4,12 +4,22 @@ defmodule ApiExample.UserController do
   def index(conn, _params) do
     users = Repo.all(ApiExample.User)
 
-    json conn, users
+    json conn_with_status(conn, users), users
   end
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(ApiExample.User, String.to_integer(id))
 
-    json conn, user
+    json conn_with_status(conn, user), user
+  end
+
+  defp conn_with_status(conn, nil) do
+    conn
+      |> put_status(:not_found)
+  end
+
+  defp conn_with_status(conn, _) do
+    conn
+      |> put_status(:ok)
   end
 end
