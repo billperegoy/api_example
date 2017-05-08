570880c032c56ab9697f65897b1d280819a1520c
Bill Peregoy <bill.peregoy@endurance.com>
2017-05-07 20:00:42 -0400

    Added CRUD table and form for User

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -20,12 +20,33 @@ main =
 
 
 type alias Model =
-    { name : String }
+    { users : List User
+    , nextUserId : Int
+    }
+
+
+type alias User =
+    { id : Int
+    , name : String
+    , email : String
+    , age : Int
+    , stooge : String
+    }
+
+
+initUsers : List User
+initUsers =
+    [ User 1 "Joe" "joe@hello.com" 23 "moe"
+    , User 2 "Jack" "jack@hello.com" 12 "larry"
+    ]
 
 
 init : ( Model, Cmd Msg )
 init =
-    Model "world" ! []
+    { users = initUsers
+    , nextUserId = 3
+    }
+        ! []
 
 
 
@@ -49,8 +70,76 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    h1 []
-        [ text ("Hello " ++ (Utils.capitalize model.name)) ]
+    div [ class "container" ]
+        [ div [ class "row" ]
+            [ userTable model.users
+            , newUserForm
+            ]
+        ]
+
+
+newUserForm : Html Msg
+newUserForm =
+    div [ class "col-md-3" ]
+        [ Html.form []
+            [ div [ class "form-group" ]
+                [ label [] [ text "Name" ]
+                , input [ class "form-control" ] []
+                ]
+            , div [ class "form-group" ]
+                [ label [] [ text "Email" ]
+                , input [ class "form-control" ] []
+                ]
+            , div [ class "form-group" ]
+                [ label [] [ text "Age" ]
+                , input [ class "form-control" ] []
+                ]
+            , div [ class "form-group" ]
+                [ label [] [ text "Stooge" ]
+                , input [ class "form-control" ] []
+                ]
+            , button [ class "btn btn-primary" ] [ text "Primary" ]
+            ]
+        ]
+
+
+userTable : List User -> Html Msg
+userTable users =
+    div [ class "col-md-9" ]
+        [ table
+            [ class "table table-striped" ]
+            [ userTableHeader
+            , tbody [] (userRows users)
+            ]
+        ]
+
+
+userTableHeader : Html Msg
+userTableHeader =
+    thead []
+        [ tr []
+            [ th [] [ text "Name" ]
+            , th [] [ text "Email" ]
+            , th [] [ text "Age" ]
+            , th [] [ text "Stooge" ]
+            ]
+        ]
+
+
+userRows : List User -> List (Html Msg)
+userRows users =
+    users
+        |> List.map userRow
+
+
+userRow : User -> Html Msg
+userRow user =
+    tr []
+        [ td [] [ text user.name ]
+        , td [] [ text user.email ]
+        , td [] [ text (toString user.age) ]
+        , td [] [ text user.stooge ]
+        ]
 
 
 
